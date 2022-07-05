d707e7a35bf7391c4dc46c9e852f9cc863288023
Ashley Gyngell <ashleygyngell@gmail.com>
2022-07-05 11:26:08 +0100

    update

--- lyricle/settings.py
+++ lyricle/settings.py
@@ -54,7 +54,6 @@ INSTALLED_APPS = [
     'jwt_auth',
     'leagues',
     'dailysongs',
-    'getlyrics',
     'scores',
 ]
 
@@ -161,7 +160,7 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = 'jwt_auth.CustomUser'
 
 
-CSRF_TRUSTED_ORIGINS = ['https://git.heroku.com/eflyricle.git']
+CSRF_TRUSTED_ORIGINS = ['https://lyriclegamedb.herokuapp.com']
 
 
 django_on_heroku.settings(locals())
