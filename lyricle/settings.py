cbd212858ca8641c55333bc1a89311b8e6c84ab1
Ashley Gyngell <ashleygyngell@gmail.com>
2022-12-22 14:40:53 +0000

    new deploy

--- lyricle/settings.py
+++ lyricle/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-import django_on_heroku
+# import django_on_heroku
 from dotenv import load_dotenv
 import dj_database_url
 load_dotenv()
@@ -163,7 +163,8 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = 'jwt_auth.CustomUser'
 
 
-CSRF_TRUSTED_ORIGINS = ['https://lyriclegamedb.herokuapp.com']
+# CSRF_TRUSTED_ORIGINS = ['https://lyriclegamedb.herokuapp.com']
+CSRF_TRUSTED_ORIGINS = ['*']
 
 
 # django_on_heroku.settings(locals())
