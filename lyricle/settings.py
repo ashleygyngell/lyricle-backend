e7bd6b865334bf98afcf495bf4ebc54468eba015
Ashley Gyngell <ashleygyngell@gmail.com>
2022-12-23 15:16:38 +0000

    change to build.sh

--- lyricle/settings.py
+++ lyricle/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-import django_on_heroku
 from dotenv import load_dotenv
 import dj_database_url
 load_dotenv()
