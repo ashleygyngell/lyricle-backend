f1149e7f5b82c3fad58b09d70c5e2c699aebfbcf
Ashley Gyngell <ashleygyngell@gmail.com>
2022-12-24 17:55:06 +0000

    try

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ set -o errexit  # exit on error
 pip install -r requirements.txt
 pip install --upgrade pip
 pip install --force-reinstall -U setuptools
+pip install django
 python manage.py collectstatic --no-input
 python manage.py migrate
