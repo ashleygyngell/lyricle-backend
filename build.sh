12b173718a2e46036d6e7f21e6eeaa6a05e679c2
Ashley Gyngell <ashleygyngell@gmail.com>
2022-12-24 17:59:43 +0000

    shell add

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ pip install -r requirements.txt
 pip install --upgrade pip
 pip install --force-reinstall -U setuptools
 pip install django
-pipenv shell
+
 python manage.py collectstatic --no-input
 python manage.py migrate
+pipenv shell
