c07a5dd91bddfb3e9f4a11555c27066aced8f23f
Ashley Gyngell <ashleygyngell@gmail.com>
2023-01-20 17:11:03 +0000

    attempt to fly

--- lyricle/settings.py
+++ lyricle/settings.py
@@ -42,13 +42,13 @@ load_dotenv(BASE_DIR / '.env')
 # ALLOWED_HOSTS = ['*']
 # core/settings.py
 
-# if ENV == 'DEV':
-#   SECRET_KEY = 'django-insecure-8zeca4r0gftp@z%r%9ium+wxc@skq71ua1covj4ci+gva(yz5d'
-# else:
-#       SECRET_KEY = str(os.getenv('SECRET_KEY'))
+if ENV == 'DEV':
+  SECRET_KEY = 'django-insecure-8zeca4r0gftp@z%r%9ium+wxc@skq71ua1covj4ci+gva(yz5d'
+else:
+      SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
